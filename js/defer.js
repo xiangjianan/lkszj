a652d60ff72c7feaf7f99878eaa5b0638f2299d7
xiangjianan <xiang9872@126.com>
2022-03-15 21:03:27 +0800

    V0.0.4 发送前确认弹框

--- js/defer.js
+++ js/defer.js
@@ -2,7 +2,7 @@
 let alert_modal = document.getElementsByClassName("alert-modal")[0];
 let alert_msg = document.getElementById('alert-msg');
 // 弹窗事件
-function alertModal(msg, time) {
+function noticeModal(msg, time) {
     alert_msg.innerHTML = msg;
     alert_modal.style.display = 'block';
     alert_modal.classList.remove('animate__fadeOutDown');
@@ -23,18 +23,18 @@ send.onclick = function (e) {
     let about = document.getElementById('about').value;
     if (!author || !opus || !tel || !link || !about) {
         if (alert_modal.classList.contains('animate__fadeOutDown')) {
-            alertModal('请完整填写作品信息', 1200);
+            noticeModal('请完整填写作品信息', 1200);
         }
     } else if (author.length > 32) {
-        alertModal('作品名过长，已超出128个字符的限制', 1200);
+        noticeModal('作品名过长，已超出128个字符的限制', 1200);
     } else if (opus.length > 32) {
-        alertModal('作品名过长，已超出128个字符的限制', 1200);
+        noticeModal('作品名过长，已超出128个字符的限制', 1200);
     } else if (tel.length > 64) {
-        alertModal('联系方式过长，已超出128个字符的限制', 1200);
+        noticeModal('联系方式过长，已超出128个字符的限制', 1200);
     } else if (link.length > 32) {
-        alertModal('作品链接过长，已超出256个字符的限制', 1200);
+        noticeModal('作品链接过长，已超出256个字符的限制', 1200);
     } else if (about.length > 1024) {
-        alertModal('作品说明过长，已超出1024个字符的限制', 1200);
+        noticeModal('作品说明过长，已超出1024个字符的限制', 1200);
     } else if (!alert_modal.classList.contains('animate__fadeInDown')) {
         let request = new XMLHttpRequest();
         request.open('POST', "https://api.lks.helloxjn.com/share/v1", true);
@@ -50,7 +50,7 @@ send.onclick = function (e) {
             if (request.readyState === 4) {
                 let res = JSON.parse(request.responseText);
                 if (res.code == 1040) {
-                    alertModal('发送失败：你的请求已被过滤 (￣^￣)ゞ', 3000);
+                    noticeModal('发送失败：你的请求已被过滤 (￣^￣)ゞ', 3000);
                 } else if (res.code == 1000) {
                     document.getElementById('author').value = '';
                     document.getElementById('opus').value = '';
@@ -61,11 +61,11 @@ send.onclick = function (e) {
                     document.getElementsByClassName('go-page-input')[0].style.display = 'none';
                     document.getElementsByClassName('is-sign-up')[0].style.display = 'block';
                     document.getElementsByClassName('go-page-home')[0].onclick();
-                    alertModal('已发送：感谢投稿 (゜-゜)つロ', 3000);
+                    noticeModal('已发送：感谢投稿 (゜-゜)つロ', 3000);
                 } else if (res.status == 429) {
-                    alertModal('发送失败：发送频率过高，请稍后再试！', 3000);
+                    noticeModal('发送失败：发送频率过高，请稍后再试！', 3000);
                 } else {
-                    alertModal('发送失败：服务器在摸鱼！', 3000);
+                    noticeModal('发送失败：服务器在摸鱼！', 3000);
                 }
             }
         }
