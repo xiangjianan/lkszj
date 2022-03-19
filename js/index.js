81829df3dd40bb030ff3afe0e018494d38bcc7e7
xiangjianan <xiang9872@126.com>
2022-03-19 20:59:47 +0800

    V0.0.33 百度统计

--- js/index.js
+++ js/index.js
@@ -209,10 +209,10 @@ document.getElementsByClassName('disclaimer-btn')[0].onclick = function () {
 }
 
 // 百度统计
-let _hmt = _hmt || [];
-(function () {
-    let hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?cc0e888dcf9f87025b43b42e6fbed048";
-    let s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?cc0e888dcf9f87025b43b42e6fbed048";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
 })();
